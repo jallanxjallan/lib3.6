2bd77f52ccee7d1c0bb71598264fb2fb77c0273e
jeremy <jeremy@jeremyallan.com>
2020-02-12 08:43:04 +0700

    Test commit

--- document/convert_document.py
+++ document/convert_document.py
@@ -6,45 +6,36 @@
 #  Copyright 2019 Jeremy Allan <jeremy@jeremyallan.com>
 
 from collections import defaultdict
-from types import generatortype
 import logging
 from pathlib import Path
 import pypandoc
 
-filter_folder = Path('/home/jeremy/Library/document/filters')
-template_folder = Path('/home/jeremy/Library/document/templates')
 
+def format_kwargs(**kwargs):
+    keyword_args = defaultdict(list)
 
+    keyword_args['format'] = 'markdown'
 
+    if kwargs.get('filters'):
+        for filter_name in kwargs['filters']:
+            f = Path(filter_name)
+            keyword_args['filters'].append(str(filter_folder.joinpath(f).with_suffix('.py')))
 
+    if kwargs.get('metadata'):
+        for k,v in metadata.items():
+            keyword_args['extra_args'].append(f'--metadata={k}:{v}')
 
+    if len(output) > 0:
+        if output[0] in pypandoc.get_pandoc_formats():
+            output = output[0]
+        else:
+            keyword_args['outputfile'] = output[0]
+            output = None
 
-def format_args(func):
-    def wrapper(source, *output, **kwargs):
-        keyword_args = defaultdict(list)
+    output = 'markdown'
+    kwargs = {k:v for k,v in keyword_args.items()}
+    return str(source), output, **kwargs
 
-        keyword_args['format'] = 'markdown'
-
-        if kwargs.get('filters'):
-            for filter_name in kwargs['filters']:
-                f = Path(filter_name)
-                keyword_args['filters'].append(str(filter_folder.joinpath(f).with_suffix('.py')))
-
-        if kwargs.get('metadata'):
-            for k,v in metadata.items():
-                keyword_args['extra_args'].append(f'--metadata={k}:{v}')
-
-        #~ if len(output) > 0:
-            #~ if output[0] in pypandoc.get_pandoc_formats():
-                #~ output = output[0]
-            #~ else:
-                #~ keyword_args['outputfile'] = output[0]
-                #~ output = None
-
-        output = 'markdown'
-        kwargs = {k:v for k,v in keyword_args.items()}
-        return func(str(source), output, **kwargs)
-    return wrapper
 
 @format_args
 def convert_text(source, output, **kwargs):
